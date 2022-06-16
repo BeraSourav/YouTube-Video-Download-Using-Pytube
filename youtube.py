5f93a67a1ff720c168fc82a9b61a98d6db03fd67
Sourav Bera <95608463+BeraSourav@users.noreply.github.com>
2022-06-16 15:12:42 +0530

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -6,7 +6,7 @@ youtube_1 = YouTube(link)
 #print(youtube_1.title) //For getting the Video Title
 #print(youtube_1.thumbnail_url) // For getting the Thumbnail Link
 video = youtube_1.streams.all()  #for getting full video and audio types
-video=youtube_1.streams.filter(only_audio=True) #for getting only audio types 
+#video=youtube_1.streams.filter(only_audio=True) #for getting only audio types 
 
 vid = list(enumerate(video))
 for i in vid:
